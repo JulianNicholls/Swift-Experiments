17ae64295898dc7d180a8d7ae076aa79f4c43c86
Julian Nicholls <juliannicholls29@gmail.com>
2016-01-04 17:51:22 +0000

    Experiment 2, Larger window

---
+++ experiment2.swift
@@ -0,0 +1,29 @@
+// experiment2
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let window = NSWindow()
+
+    func applicationDidFinishLaunching(aNotification : NSNotification) {
+        window.setContentSize(NSSize(width: 600, height: 300))
+
+        window.styleMask = NSTitledWindowMask |
+                           NSClosableWindowMask |
+                           NSMiniaturizableWindowMask |
+                           NSResizableWindowMask
+
+        window.opaque = false
+        window.center()
+        window.title = "Experiment 2"
+
+        window.makeKeyAndOrderFront(window)
+        window.level = 1
+    }
+}
+
+let app = NSApplication.sharedApplication()
+let controller = AppDelegate()
+
+app.delegate = controller
+app.run()
